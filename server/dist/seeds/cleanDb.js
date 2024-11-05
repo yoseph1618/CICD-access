f1dae647dc3206809005f792e7376551277bd612
Luke Skywalker <yoseph16181@gmail.com>
2024-11-04 21:41:45 -0600

    add gitignore.

--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -2,10 +2,10 @@ import models from '../models/index.js';
 import db from '../config/connection.js';
 export default async (modelName, collectionName) => {
     try {
-        let modelExists = await models[modelName].db.db.listCollections({
+        let modelExists = await models[modelName].db?.db?.listCollections({
             name: collectionName
         }).toArray();
-        if (modelExists.length) {
+        if (modelExists?.length) {
             await db.dropCollection(collectionName);
         }
     }
