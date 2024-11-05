bfeee5eaa97fa92cec049d627c2c0658a89161f8
Luke Skywalker <yoseph16181@gmail.com>
2024-11-05 15:02:46 -0600

    updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,13 +12,13 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+// if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
    // app.get('*', (_req, res) => {
    //  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   // });
-}
+// }
+app.use(routes);
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
 });
