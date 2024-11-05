9a3f335c7cbf5aa01b89187484951fb8be035306
Luke Skywalker <yoseph16181@gmail.com>
2024-11-05 14:22:39 -0600

    updates.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+import { fileURLToPath } from 'node:url'; 
+import { dirname } from 'path';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
 await db;
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,9 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
    app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
 app.listen(PORT, () => {
