469ac86d8094227a3a9b29adb0b878d99f8e664c
Luke Skywalker <yoseph16181@gmail.com>
2024-11-05 14:03:22 -0600

    Update package.json and tsconfig.

--- server/dist/seeds/seed.js
+++ server/dist/seeds/seed.js
@@ -1,7 +1,7 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+import pythonQuestions from './pythonQuestions.json' with { type: "json" };
 db.once('open', async () => {
     await cleanDB('Question', 'questions');
     await Question.insertMany(pythonQuestions);
