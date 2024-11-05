0861f2bd9bc96af593efd74f11f1d584e60c8218
Luke Skywalker <yoseph16181@gmail.com>
2024-11-05 14:13:42 -0600

    updates.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,19 @@
 import express from 'express';
-// import path from 'node:path';
+import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
-
-const app = express();
+await db;
 const PORT = process.env.PORT || 3001;
-
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
-
 app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+   app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
 });
